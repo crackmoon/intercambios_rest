94315a2684bfd16366e45672e93b9768959e65ec
mchaparro <mx.mik3@gmail.com>
2013-11-24 17:57:13 -0700

    heroku config

--- app.js
+++ app.js
@@ -192,4 +192,9 @@ function formatDate(date) {
 }
 
 // Launch server
-app.listen(1212);
+//app.listen(5000);
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
