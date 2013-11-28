f0f79c261dcafb8a5bbf6f89d471c434ce2192a1
mchaparro <mx.mik3@gmail.com>
2013-11-27 18:30:59 -0700

    fix en los metodos

--- app.js
+++ app.js
@@ -23,15 +23,36 @@ port: 3306
 });
 
 
-// var connection = mysql.createConnection({
-// host : 'localhost',
-// user : 'root',
-// password : 'root',
-// database: 'intercambios',
-// port: '8889'
-// });
+ // var connection = mysql.createConnection({
+ // host : 'localhost',
+ // user : 'root',
+ // password : 'root',
+ // database: 'intercambios',
+ // port: '8889'
+ // });
 
 // Configuracion inicial
+
+
+//Cros site stuff!!! o__o!!!!
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.configure(function () {
   app.use(express.bodyParser());
@@ -44,62 +65,70 @@ app.configure(function () {
 app.get('/', function (req, res) {
   res.send('Api Intercambios REST:</br>'+
   			'<ul>'+
-        '<h3>Metodos get:</h3>'+
-  			'<li>Lista de todos los eventos              : /get/eventos </li>'+
-  			'<li>Lista de todos los eventos de un usuario : /get/eventos/usuario/:id </li>'+
-  			'<li>Lista de todos usuarios				  : /get/usuarios </li>'+
-        '<li>Lista de todos los eventos del administrador : /get/eventos/admin/:id </li>'+
-        '<li>Muestra los detalles de un evento : get/evento/:id'+
-        '<h3>Metodos post</h3>'+
-        '<li>Crear un nuevo evento          : /post/evento </li>'+
+        '<h3>Metodos GET:</h3>'+
+  			'<li>Lista de todos los eventos              : eventos </li>'+
+  			'<li>Lista de todos los eventos de un usuario : eventos/usuario/:id </li>'+
+  			'<li>Lista de todos usuarios				  : usuarios </li>'+
+        '<li>Lista de todos los eventos del administrador : eventos/admin/:id </li>'+
+        '<li>Muestra los detalles de un evento : evento/:id'+
+        '<h3>Metodos POST:</h3>'+
+        '<li>Crear un nuevo evento          : evento </li>'+
         '<div>{"creador":id,"nombre":"nombre","fecha":"2013/12/24","participantes":99,"precio":100}</div>'+
-        '<h3>Metodos put</h3>'+
-        '<li>Actualiza la informacion de un evento : /update/evento/:id</li>'+
+        '<h3>Metodos PUT:</h3>'+
+        '<li>Actualiza la informacion de un evento : evento/:id</li>'+
         '<div>{"creador":id,"nombre":"nombre","fecha":"2013/12/24","participantes":99,"precio":100}</div>'+
   			'</ul>'
   			);
 });
 
 
+/*************************/
+//       Metodos GET     
+/*************************/
+
 //Regresa todos los usuarios registrados en la aplicación
-app.get('/get/usuarios', function (req, res) {
+app.get('/usuarios', function (req, res) {
    connection.query('SELECT id,nombre,email,fecha FROM intercambios_usuario;', function (error, rows, fields) { 
+      console.log(error);
          res.writeHead(200, { 'Content-Type': 'application/json' });
 		 res.end(JSON.stringify(rows));
       }); 
 });
 
 //Regresa todos los eventos registrados en la aplicación
-app.get('/get/eventos', function (req, res) {
+app.get('/eventos', function (request, res) {
+    //res.writeHead(200, {'Content-Type': 'application/json'}); 
    connection.query('SELECT * FROM intercambios_evento;', function (error, rows, fields) { 
-    console.log(error);
-         res.writeHead(200, { 'Content-Type': 'application/json' });
-		 res.end(JSON.stringify(rows));
-      }); 
+   
+    res.header('Access-Control-Allow-Origin', "*");    
+    res.end( JSON.stringify(rows));
+    
+   });
+		 
+});
+
+//Regresa un evento
+app.get('/evento/:id', function (req, res){
+  connection.query('SELECT * FROM intercambios_evento where id ='+req.params.id+';', function (error, rows, fields) {
+      res.header('Access-Control-Allow-Origin', "*");    
+      res.end( JSON.stringify(rows[0]));
+    
+  }); 
 });
 
 //Regresa todos los eventos de un usuario en especifico
-app.get('/get/eventos/usuario/:id', function (req, res){
+app.get('/eventos/usuario/:id', function (req, res){
 	connection.query('SELECT * FROM intercambios_evento where id in (SELECT evento_id   FROM intercambios_participantesevento WHERE usuario_id='+req.params.id+' and estado="activo")', function (error, rows, fields) {
  
  
           res.writeHead(200, {'Content-Type': 'application/json'});
-		 res.end(JSON.stringify(rows));
+		 res.end(JSON.stringify(rows[0]));
 		
       }); 
 });
 
-app.get('/get/evento/:id', function (req, res){
-  connection.query('SELECT * FROM intercambios_evento where id ='+req.params.id+';', function (error, rows, fields) {
- 
- 
-          res.writeHead(200, {'Content-Type': 'application/json'});
-     res.end(JSON.stringify(rows));
-    
-      }); 
-});
-
-app.get('/get/eventos/admin/:id', function (req, res){
+//Regresa todos los eventos donde el usuario es admin
+app.get('/eventos/admin/:id', function (req, res){
   connection.query('SELECT * FROM intercambios_evento where admin_id ='+req.params.id, function (error, rows, fields) { 
          res.writeHead(200, {'Content-Type': 'application/json'});
      res.end(JSON.stringify(rows));
@@ -107,9 +136,12 @@ app.get('/get/eventos/admin/:id', function (req, res){
       }); 
 });
 
+/*************************/
+//       Metodos POST     
+/*************************/
 
-
-app.post('/post/evento', function(req, res) {
+//Crea un nuevo evento
+app.post('evento', function(req, res) {
   if(!req.body.hasOwnProperty('nombre') || 
      !req.body.hasOwnProperty('fecha')) {
     res.statusCode = 400;
@@ -131,29 +163,24 @@ app.post('/post/evento', function(req, res) {
   var creador = req.body.creador;
   var activo = "activo";
   
-  //console.log('insert into intercambios_evento ( admin_id, nombre , fecha_evento, numero_participantes, precio, fecha_creacion,estado ) values (2,' + "'"+ nombre +"'" +',' + "'"+ fecha +"'" +',' + "'"+ participantes +"'" +',' + "'"+ precio +"',NOW()" +"'"+activo+"'"+');');
+  
   connection.query('INSERT into intercambios_evento ( admin_id, nombre , fecha_evento, numero_participantes, precio, fecha_creacion,estado ) values ('+"'"+creador+"'"+',' + "'"+ nombre +"'" +',' + "'"+ fecha +"'" +',' + "'"+ participantes +"'" +',' + "'"+ precio +"',NOW()," +"'"+activo+"'"+');', function (error, rows, fields) {  
- // console.log('INSERT into intercambios_participantesevento (usuario_id, evento_id, fecha ) values ('+"'"+creador+"'"+','+"'"+rows['insertId']+"',NOW()"+');');
-  connection.query('INSERT into intercambios_participantesevento (usuario_id, evento_id, fecha ) values ('+"'"+creador+"'"+','+"'"+rows['insertId']+"',NOW()"+');', function (error, rows, fields) { 
-  //    console.log(error);
-    console.log(error);
-      //console.log(error);
-     console.log(JSON.stringify(rows));
-       
-     // console.log(evento_id);
-
-     res.writeHead(200, {'Content-Type': 'application/json'});
-     res.end(JSON.stringify(rows));
-    
+      connection.query('INSERT into intercambios_participantesevento (usuario_id, evento_id, fecha ) values ('+"'"+creador+"'"+','+"'"+rows['insertId']+"',NOW()"+');', function (error, rows, fields) { 
+         res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(rows));
       }); 
-    
-    
-          }); 
-
-
+  }); 
 
 });
-app.put('/update/evento/:id', function(req, res) {
+
+
+/*************************/
+//       Metodos PUT     
+/*************************/
+
+//Actualiza la informacion de un evento
+app.put('/evento/:id', function(req, res) {
+
   //fecha debe tener este formato: YYYY/mm/ddd ej: 2013/12/24
   if(!req.body.hasOwnProperty('nombre') || 
   //   !req.body.hasOwnProperty('fecha')) ||
@@ -170,17 +197,14 @@ app.put('/update/evento/:id', function(req, res) {
   }
 
   var nombre = req.body.nombre;
-  var fecha = new Date(req.body.fecha);
-  var fecha = formatDate(fecha);
-  var participantes = req.body.participantes;
+  //var fecha = new Date(req.body.fecha);
+  //var fecha = formatDate(fecha);
+  var participantes = req.body.numero_participantes;
   var precio = req.body.precio;
 
-  connection.query('update intercambios_evento set nombre = "' + nombre +'", fecha_evento = "'+fecha+'", numero_participantes = "'+participantes+'", precio = "'+precio+'" where id= '+req.params.id+ ';',function (error, rows, fields) {
-   console.log(error);
-   
-    
-    
-          }); 
+  //connection.query('update intercambios_evento set nombre = "' + nombre +'", fecha_evento = "'+fecha+'", numero_participantes = "'+participantes+'", precio = "'+precio+'" where id= '+req.params.id+ ';',function (error, rows, fields) {
+  connection.query('update intercambios_evento set nombre = "' + nombre +'", numero_participantes = "'+participantes+'", precio = "'+precio+'" where id= '+req.params.id+ ';',function (error, rows, fields) {
+  }); 
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Se modifico correctamente el evento:'+req.params.id);
@@ -188,6 +212,7 @@ app.put('/update/evento/:id', function(req, res) {
 
 });
 
+//Cancela un evento
 app.put('/cancelar/evento/:id', function(req, res) {
   
 
@@ -211,6 +236,9 @@ app.put('/cancelar/evento/:id', function(req, res) {
 });
 
 
+/*************************/
+//       Metodos DELETE     
+/*************************/
 
 
 //Formateo para poder guardar fechas en mysql
